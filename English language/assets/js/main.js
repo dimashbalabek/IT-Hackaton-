3eb8b0a18f734d38c4f21e20190324400f0b0e82
dimashbalabek <dimashbalabek0@gmail.com>
2024-03-28 04:18:25 +0600

    added language

--- English language/assets/js/main.js
+++ English language/assets/js/main.js
@@ -125,7 +125,7 @@ chk.addEventListener('change', () => {
         const shadow2_png = document.querySelector(".home__shadow");
     
         // Change color of headings, paragraphs, and links to black for light mode
-        const headings = document.querySelectorAll('h1, h2, h3');
+        const headings = document.querySelectorAll('h1, h2, h3, a');
         const paragraphs = document.querySelectorAll('p');
         const links = document.querySelectorAll('a');
         
@@ -145,4 +145,51 @@ chk.addEventListener('change', () => {
         shadow2_png.style.background = "linear-gradient(180deg, hsl(0, 0%, 100%) 5%, hsla(0, 0%, 100%, 0) 40%, hsla(0, 0%, 100%, 0) 60%, hsl(0, 0%, 100%) 92%)";
         index = 1; // Сброс индекса для обозначения активной темы
     }
+});
+
+/*===== LANGUAGE SWAP =====*/
+
+document.addEventListener('DOMContentLoaded', () => {
+	/*
+		1. по клику на пункты верхнего меню открывать дропдаун
+		2. по клику (повторному) на эти пункты - закрывать дропдаун
+		3. по клику в любое место сайта, кроме меню - закрывать дропдаун
+	*/
+
+	const menuBtns = document.querySelectorAll('.menu__btn');
+	const drops = document.querySelectorAll('.dropdown');
+
+	menuBtns.forEach(el => {
+		el.addEventListener('click', (e) => {
+			let currentBtn = e.currentTarget;
+			let drop = currentBtn.closest('.menu__item').querySelector('.dropdown');
+
+			menuBtns.forEach(el => {
+				if (el !== currentBtn) {
+					el.classList.remove('menu__btn--active');
+				}
+			});
+
+			drops.forEach(el => {
+				if (el !== drop) {
+					el.classList.remove('dropdown--active');
+				}
+			});
+
+			drop.classList.toggle('dropdown--active');
+			currentBtn.classList.toggle('menu__btn--active');
+		});
+	});
+
+	document.addEventListener('click', (e) => {
+		if (!e.target.closest('.menu')) {
+			menuBtns.forEach(el => {
+				el.classList.remove('menu__btn--active');
+			});
+
+			drops.forEach(el => {
+				el.classList.remove('dropdown--active');
+			});
+		}
+	});
 });
